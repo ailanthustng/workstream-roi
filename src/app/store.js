2be8a5a9574933483a4b20d55e0061e93d35685e
ailanthustng <ailanthustng@gmail.com>
2020-11-15 02:32:48 +0800

    remove redundant lines

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
 import roiReducer from '../features/ROICalculator/roiSlice'
 
 export default configureStore({
   reducer: {
-    counter: counterReducer,
     roi: roiReducer
   },
 });
